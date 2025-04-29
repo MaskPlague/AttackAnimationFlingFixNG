4f6b2c442937281d8d5b60638b5a090eb078076d
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-04-29 04:00:33 -0500

    import changes from ledge block

--- src/pch.h
+++ src/pch.h
@@ -2,5 +2,9 @@
 
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
+#include <spdlog/sinks/basic_file_sink.h>
+#include <windows.h>
+
+#define DLLEXPORT __declspec(dllexport)
 
 using namespace std::literals;
